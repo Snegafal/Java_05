6e1ca7df3104cff7ce333f9ae71e3b2615937830
Snezhana <S_ne_ga@mail.ru>
2022-11-05 14:58:22 +0300

    added CreateArray Algorithm

--- src/test/java/BiggerValueTest.java
+++ src/test/java/BiggerValueTest.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class BiggerValueTest {
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class BiggerValueTest {
+
+    @Test
+    public void testFirstNumberIsLower () {
+        int number1 = 3333;
+        int number2 = 9999;
+
+        int expectedResult = number2;
+
+        int actualResult = new BiggerValue().getBiggerValueOutOfTwoNumbers(number1, number2);
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void testSecondNumberIsLower () {
+        int number1 = 3333;
+        int number2 = 1111;
+
+        int expectedResult = number1;
+
+        int actualResult = new BiggerValue().getBiggerValueOutOfTwoNumbers(number1, number2);
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void testEqualNumbers () {
+        int number1 = 9999;
+        int number2 = 9999;
+
+        int expectedResult = number2;
+
+        int actualResult = new BiggerValue().getBiggerValueOutOfTwoNumbers(number1, number2);
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
 }
